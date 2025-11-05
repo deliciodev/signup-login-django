e6aab71bd3be8cca71257e1d75ce1b79d36ae066
Delicio M <deliciodev@gmail.com>
2025-11-05 06:48:03 -0300

    customers/models set

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class Customer(AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=25, unique=True)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name']
+    class Meta:
+        verbose_name_plural = 'Customers'
+
+    def __str__(self):
+        return self.name
