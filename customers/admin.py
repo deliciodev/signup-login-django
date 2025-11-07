4d19bfbf446abcb7bd7acd4f93cd61ed096596b8
Delicio M <deliciodev@gmail.com>
2025-11-07 01:57:50 -0300

    feat: add customers/admin.py

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(BaseUserAdmin):
+    ordering = ("created_at",)
+    list_display = ("email", "username", "name", "is_active", "is_staff", "created_at")
+    search_fields = ("email", "username", "name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "username", "name", "password")}),
+        ("Personal info", {"fields": ("email", "username", "name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "created_at")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "name", "password1", "password2"),
+        }),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly = super().get_readonly_fields(request, obj)
+        return readonly + ("created_at",)
