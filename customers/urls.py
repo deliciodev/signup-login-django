669d66bc1fab23a766f992b02530fceed60cfad5
Delicio M <deliciodev@gmail.com>
2025-11-07 01:47:48 -0300

    fix: fix customers urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,18 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
+app_name = 'customers'
 
 urlpatterns = [
-    path('customers/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('customers/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('customers/token/refresh/', views.JWTTokenRefreshView.as_view(), name='customer_token_refresh'),
 
     path('customers/', views.CustomerCreateListView.as_view(), name='customer_create_list'),
     path('customers/signup/', views.CustomerSignupView.as_view(), name='customer_signup'),
     path('customers/login/', views.CustomerLoginView.as_view(), name='customer_login'),
     path('customers/logout/', views.CustomerLogoutView.as_view(), name='customer_logout'),
     path('customers/<uuid:pk>/', views.CustomerRetrieveUpdateDestroyView.as_view(), name='customer_retrieve_update_destroy'),
+    path('customers/profile/', views.CustomerProfileView.as_view(), name='customer_profile'),
 ]
